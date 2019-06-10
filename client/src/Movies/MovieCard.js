95635b222974e6db9fadbe89481ce94313e0b486
Rory Murray <rory.christian.murray@gmail.com>
2019-06-10 17:58:52 -0400

    MVP commit

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const MovieCard = props => {
-  return;
-};
+export default function MovieDetails({ movie }) {
+  const { title, director, metascore, stars, id } = movie;
+  console.log("MovieCard movie", movie);
+  return (
+    <div className="movie-card">
+      <Link to={`/movies/${id}`}>
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
 
-export default MovieCard;
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </Link>
+    </div>
+  );
+}
